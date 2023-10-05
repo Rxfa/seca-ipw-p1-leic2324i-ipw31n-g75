ba65dcbc9445ee873417ad683b05f73a1b7ea60d
rxfa <rafa3rnrn@gmail.com>
2023-10-05 16:45:56 +0100

    Solved 5.js

--- a1/5.js
+++ a1/5.js
@@ -1,1 +1,43 @@
+import assert from "assert";
 
+var goodUsers = [
+    { id: 1 },
+    { id: 2 },
+    { id: 3 }
+ ]
+ 
+ // `checkUsersValid` is the function you'll define
+ var testAllValid = checkUsersValid(goodUsers)
+
+ function checkUsersValid(validUsers){
+    return function(givenUsers){
+        const arr = validUsers.map(item => JSON.stringify(item))
+        return givenUsers.every(user => arr.includes(JSON.stringify(user)))
+    }
+ }
+ 
+ /*
+    testAllValid([
+        { id: 2 },
+        { id: 1 }
+    ])
+    // => true
+    
+    testAllValid([
+        { id: 2 },
+        { id: 4 },
+        { id: 1 }
+    ])
+    // => false
+ */
+
+assert.deepEqual( testAllValid([
+    { id: 2 },
+    { id: 4 },
+    { id: 1 }
+ ]), false)
+
+assert.deepEqual( testAllValid([
+    { id: 2 },
+    { id: 1 }
+ ]), true)
